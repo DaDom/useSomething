59bd6184b36c9ca67fec43a6a3ac38fc7b40b990
Dominik Picker <dominik.pickerh@gmail.com>
2021-01-02 16:15:44 +0200

    change

--- useSomething.js
+++ useSomething.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 
-export default function useTextSelection(ref) {
+export default function useSomething(ref) {
     const [range, setRange] = useState(null);
 
     useEffect(() => {
