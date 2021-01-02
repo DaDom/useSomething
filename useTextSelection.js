9a683109de2a1666eefd6d3bfe09926a6e1add34
Dominik Picker <dominik.pickerh@gmail.com>
2021-01-02 16:01:47 +0200

    initial commit

---
+++ useTextSelection.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+
+export default function useTextSelection(ref) {
+    const [range, setRange] = useState(null);
+
+    useEffect(() => {
+        if (ref && ref.current) {
+            const checkSelection = () => {
+                const currentSelection = window.getSelection();
+                if (!currentSelection.isCollapsed && currentSelection.rangeCount > 0) {
+                    const currentRange = currentSelection.getRangeAt(0);
+                    const containsStart = ref.current.contains(currentRange.startContainer);
+                    const containsEnd = ref.current.contains(currentRange.endContainer);
+                    if (containsStart && containsEnd) {
+                        setRange(currentRange);
+                    }
+                } else {
+                    setRange(null);
+                }
+            };
+
+            document.addEventListener('selectionchange', checkSelection, { passive: true });
+            return () => document.removeEventListener('selectionchange', checkSelection);
+        }
+        return undefined;
+    }, [ref]);
+
+    return range;
+}
+
